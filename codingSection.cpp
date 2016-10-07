db091e6454f052b08cb19ec494562000af706ca0
Josh <joshleidl@gmail.com>
2016-10-06 23:44:00 -0600

    The start of the input class and breakdown

--- codingSection.cpp
+++ codingSection.cpp
@@ -3,12 +3,36 @@
 
 using namespace std;
 
+class inputNode
+{
+    private:
+        inputNode *next;
+        int value;
+        char op;
+        bool flag;
+        
+    public:
+        inputNode() {next = NULL; value = 0; op = ' '; flag = false;}
+        inputNode(int i) {next = NULL; value = i; op = ' '; flag = true;}
+        inputNode(char c) {next = NULL; value = 0; op = c; flag = false;}
+        void setNext(inputNode* n) {next = n;}
+        inputNode* getNext() {return next;}
+        bool hasInt() {return flag;}
+        char getChar() {return op;}
+        void setChar(char c) {op = c; flag = false;}
+        int getInt() {return value;}
+        void setInt(int i) {value = i; flag = true;}
+};
+
 string infixToPostfix(string toConvert)
 {
     //I'll start with a basic case
+    return " ";
 }
 
 int main()
 {
-       
+    string input;
+    
+    cout << "Please enter an inline mathematical expression: " << endl;
 }

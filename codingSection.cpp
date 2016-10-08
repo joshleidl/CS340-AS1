3325551992088cd98f419bb744be04e022a1df68
Josh <joshleidl@gmail.com>
2016-10-07 20:51:19 -0600

    tokenizer function implemented but requires testing

--- codingSection.cpp
+++ codingSection.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+
+#define ISINT true
+#define NOTINT false
 
 using namespace std;
 
@@ -13,8 +17,8 @@ class inputNode
         
     public:
         inputNode() {next = NULL; value = 0; op = ' '; flag = false;}
-        inputNode(int i) {next = NULL; value = i; op = ' '; flag = true;}
-        inputNode(char c) {next = NULL; value = 0; op = c; flag = false;}
+        inputNode(int i, char c, bool isInt);
+        ~inputNode(); //TODO write this
         void setNext(inputNode* n) {next = n;}
         inputNode* getNext() {return next;}
         bool hasInt() {return flag;}
@@ -24,21 +28,101 @@ class inputNode
         void setInt(int i) {value = i; flag = true;}
 };
 
+inputNode::inputNode(int i, char c, bool isInt)
+{
+    if(isInt)
+    {
+        value = i;
+        op = ' ';
+    }
+    else
+    {
+        op = c;
+        value = 0;
+    }
+    
+    flag = isInt;
+}
+
 class nodeList
 {
     private:
-        inputNode *head;
+        inputNode* head,* tail;
         
     public:
-        nodeList() {head = NULL;}
-        nodeList(inputNode* h) {head = h;}
+        nodeList() {head = NULL; tail = NULL;}
+        nodeList(inputNode* h) {head = h; tail = h;}
+        ~nodeList(); //TODO write this
         inputNode* getHead() {return head;}
         void setHead(inputNode* h) {head = h;}
+        inputNode* getTail() {return tail;}
+        void setTail(inputNode* t) {tail = t;}
+        void addNode(inputNode* i);
+        
 };
 
-inputNode* tokenizeString(string s)
+void nodeList::addNode(inputNode* i)
 {
+    if (head == NULL)
+    {
+        head = i;
+        tail = i;
+    }
+    else
+    {
+        tail->setNext(i);
+        tail = tail->getNext();
+    }
+}
+
+nodeList* tokenizeString(string s)
+{
+    int counter = 0;
+    int value;
+    int maxIndex = s.length();
+    string section;
+    nodeList* toReturn;
+    inputNode* toAdd;
     
+    while(s.length() > 0)
+    {
+        while(counter + 1 < s.length() && s[counter] != ' ')
+        {
+            counter++;
+        }
+        
+        section = s.substr(0, counter - 1);
+        
+        switch(section[0])
+        {
+            case '+':
+            case '*':
+            case '/':
+                //TODO maybe throw error?
+                //check for faulty input
+                toAdd = new inputNode(0, section[0], NOTINT);
+                break;
+            
+            case '-':
+                if(section.length() > 2)
+                {
+                    //Case of faulty input or negative number
+                    istringstream buffer(section);
+                    buffer >> value;
+                    toAdd = new inputNode(value , ' ', ISINT);
+                }
+                else
+                {
+                    toAdd = new inputNode(0, section[0], NOTINT);
+                }
+                break;
+                
+            default:
+                //Throw exception
+                break;
+        }
+                
+    }
 }
 
 int main()
